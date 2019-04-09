e92101d57773448a4ae104b39276a2277d535610
Bisri Mustofa <agustoreformansyah17@gmail.com>
2019-04-09 20:58:21 +0700

    1617051073

---
+++ pushNotif/app/src/main/java/i/akbar/pushnotif/User.java
@@ -0,0 +1,13 @@
+package i.akbar.pushnotif;
+
+public class User {
+
+    public String email;
+    public String token;
+
+    public User(String email, String token){
+        this.email = email;
+        this.token = token;
+    }
+
+}
